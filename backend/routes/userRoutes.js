85583d4c4b27f146f2cfb9c4f8bc613436da0588
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-09-19 01:08:59 +0300

    authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET ;
